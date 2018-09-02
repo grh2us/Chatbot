d01ba195ac980c54ccc748e3e72b2f6a26eba201
okkhoury <okk6nb@virginia.edu>
2018-09-02 18:20:57 -0400

    shits still fucked

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ var dbConfig = require('./config/db');
 var bodyParser = require('body-parser');
 var Validator = require('jsonschema').Validator;
 
-
 /*Global variable to store instance of mongoDB*/
 var database_glb = null;
 
@@ -25,7 +24,11 @@ var validation_resp = null;
 /*Instance of Validator*/
 var v = new Validator();
 
-/*Array to store all the websockets*/
+/*Map of sockets to client_info*/
+var socketsMap = {};
+
+// Global request variable
+var req_glb = null;
 
 /*Allows us to access fields of data in get/post request parameter*/
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -49,114 +52,170 @@ app.get('/', function (req, res) {
 
 /*Method to handle post request from login page, gets called after hitting submit button*/
 app.post('/chat', function (req, res) {
-	console.log(req.body);
-	res.sendFile(__dirname + '/views/chat.html');
-  chatbot(database_glb,req);
+  //console.log(req.body);
+  res.sendFile(__dirname + '/views/chat.html');
+  req_glb = req;
+  //chatbot(database_glb,req);
 });
 
 /*Create http server on port 3000*/
 http.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  //console.log('Example app listening on port 3000!')
 });
 
+/*Listener for creation of websockets*/
+io.on('connection',function(socket){
+  chatbot(req_glb,socket);
+})
+
 /*Contains all the logic for chatbot*/
-function chatbot(database, req){
+function chatbot(req,socket){
 
-  /*Listener for websocket connection*/
-	io.on('connection', function(socket){
+  // Only run if a request has been made
+  if (req != null) {
 
-		console.log('User ' + req.body.uname + ': A new WebSocket connection has been established');
+    //var localSocket = socket;
 
-    client_info={
-      uname: req.body.uname,
-      psw : req.body.psw,
-      state: 0,
-      isDone: 0,
-      attributes: [],
-      fields: []
-    };
+    // console.log("CHATBOT FUNCTION CALLED");
 
-    /*Listener for chat message event*/
-		socket.on('chat message', function(msg){
+    // console.log(socket.id);
+
+    if (socketsMap[socket.id] == undefined) {
+
+      //console.log("NEW SOCKET ADDED");
+
+      client_info = {
+        uname: req.body.uname,
+        psw : req.body.psw,
+        state: 0,
+        isDone: 0,
+        attributes: [],
+        fields: []
+      };
+
+      socketsMap[socket.id] = client_info;
+
+      //socketsMap.set(socket, client_info);
+    }
+
+    // console.log("Socket ID:" + socket.id);
+
+    // console.log('User ' + req.body.uname + ': A new WebSocket connection has been established');
+
+    // console.log("Size " + Object.keys(socketsMap).length);
+
+    for(var s in socketsMap) {
+      console.log("MAP: " + JSON.stringify(s) + ", " + JSON.stringify(socketsMap[s]));
+    }
+
+    for(var socketMapId in socketsMap) {
+      // console.log("Map value: " + socketMapId);
+      // console.log("Current socket value: " + socket);
+      if(socketMapId == socket.id){
+        console.log("Using socket: " + socket.id)
+        socketLogic(socket);
+      }
+    }
+
+  }
+}
+
+function socketLogic(socket) {
+
+  console.log("Calling socket logic");
+  console.log("SocketLogic: " + socket.id);
+
+  client_info = socketsMap[socket.id]
+
+  console.log("Current user: " + client_info.uname);
+  console.log(client_info);
+  console.log('------------------------')
+
+  /*Listener for chat message event*/
+  socket.on('chat message', function(msg){
     
-      var db = database.db("test-api-db");
+    console.log(client_info);
+    
+    var db = database_glb.db("test-api-db");
 
-      /*Using promise to ensure that questions are loaded from DB before iterating through them*/
-      var promise = new Promise(function(resolve,reject){
-        db.collection('Questions').find({}, function(err,result){
-          if (err) {
-            reject(err);
+    /*Using promise to ensure that questions are loaded from DB before iterating through them*/
+    var promise = new Promise(function(resolve,reject){
+      db.collection('Questions').find({}, function(err,result){
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result.toArray());
+        }
+      });
+    }).then(function(result){
+      /*Validate the message*/
+      if (schema != null){
+
+        /*Returns 1 if there are any errors*/
+        var err = v.validate(msg,schema).errors.length;
+
+        /*Assign value to valid msg*/
+        valid_msg = err;
+      }
+
+      /*Checking if message is valid*/
+      if (valid_msg == 0){
+
+        /*Iterating through all the questions and choosing what to ask*/
+        for(var i=0; i<result.length; i++){
+
+          /*Making sure question state asked matches current state of client*/
+          if (client_info.state == result[i].state){
+
+            /*Grab validation info from this question*/
+            schema = result[i].schema;
+            validation_resp = result[i].validation;
+          
+            /*Sending message on websocket*/
+            socket.emit('chat message', result[i].question);
+
+            /*Update client_info object*/
+            client_info.state++;
+            client_info.isDone = result[i].isDone;
+            client_info.attributes.push(msg);
+            client_info.fields.push(result[i].field);
+
+            // Update the state of the socket.
+            console.log("Updating info");
+            socketsMap[socket.id] = client_info;
+            console.log(client_info);
+            console.log('-------------------------------');
+
+            break;
+          }
+        }
+
+      } else {
+        /*Message waas invalid so send validation message back to client*/
+        socket.emit('chat message', validation_resp);
+      }
+
+      /*Inserting client_info object into DB*/
+      if(client_info.isDone == 1){
+        var db = database_glb.db("test-api-db");
+        console.log(client_info);
+        db.collection('Users').insert(client_info, function(err,result){
+          if(err){
+            console.log(err);
           } else {
-            resolve(result.toArray());
+            console.log('Sucessfully inserted information into database');
+            socket.disconnect(0);
           }
         });
-      }).then(function(result){
-        /*Validate the message*/
-        if (schema != null){
+      }
 
-          /*Returns 1 if there are any errors*/
-          var err = v.validate(msg,schema).errors.length;
+    },function(err){
+        console.log(err);
+    })
+  });
 
-          /*Assign value to valid msg*/
-          valid_msg = err;
-        }
-
-        /*Checking if message is valid*/
-        if (valid_msg == 0){
-
-          /*Iterating through all the questions and choosing what to ask*/
-          for(var i=0; i<result.length; i++){
-
-            /*Making sure question state asked matches current state of client*/
-            if (client_info.state == result[i].state){
-
-              /*Grab validation info from this question*/
-              schema = result[i].schema;
-              validation_resp = result[i].validation;
-            
-              /*Sending message on websocket*/
-              socket.emit('chat message', result[i].question);
-
-              /*Update client_info object*/
-              client_info.state++;
-              client_info.isDone = result[i].isDone;
-              client_info.attributes.push(msg);
-              client_info.fields.push(result[i].field);
-              break;
-            }
-          }
-
-        } else {
-          /*Message waas invalid so send validation message back to client*/
-          socket.emit('chat message', validation_resp);
-        }
-
-        /*Inserting client_info object into DB*/
-        if(client_info.isDone == 1){
-          var db = database_glb.db("test-api-db");
-          console.log(client_info);
-          db.collection('Users').insert(client_info, function(err,result){
-            if(err){
-              console.log(err);
-            } else {
-              console.log('Sucessfully inserted information into database');
-              socket.disconnect(0);
-            }
-          });
-        }
-
-      },function(err){
-          console.log(err);
-      })
-
-		});
-
-    /*Listener for disconnect i.e. when client closes browser window*/
-		socket.on('disconnect', function(){
-    		console.log('User ' + req.body.uname + ' has disconnected');
-  		});
-    
-	});
-
-
+  /*Listener for disconnect i.e. when client closes browser window*/
+  // socket.on('disconnect', function(){
+  //   console.log('User ' + req.body.uname + ' has disconnected');
+  // });
 }
